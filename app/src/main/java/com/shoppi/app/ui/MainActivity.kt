43183f73791e86fc43b60d303bc7b02831029d18
dpwns1234 <dpwns1234@naver.com>
2022-02-25 20:25:45 +0900

    Initial commit

---
+++ app/src/main/java/com/shoppi/app/ui/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.shoppi.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.shoppi.app.R
+
+private const val TAG = "class name"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
+
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
+
+    }
+
+
+    class CollectionFragment : Fragment() {
+
+    }
+}
